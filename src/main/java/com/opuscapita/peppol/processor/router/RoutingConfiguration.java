768c58e90fd3f053156a729e1808e9f422ef018f
thebilge <ibrahimbilge7@gmail.com>
2019-07-08 13:35:05 +0200

    adding back routing config

--- src/main/java/com/opuscapita/peppol/processor/router/RoutingConfiguration.java
+++ src/main/java/com/opuscapita/peppol/processor/router/RoutingConfiguration.java
@@ -2,14 +2,12 @@ package com.opuscapita.peppol.processor.router;
 
 import com.opuscapita.peppol.commons.container.state.Route;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@RefreshScope
 @ConfigurationProperties(prefix = "peppol.routing-config")
 public class RoutingConfiguration {
 
--- src/test/java/com/opuscapita/peppol/processor/util/CleanTest.java
+++ src/test/java/com/opuscapita/peppol/processor/util/CleanTest.java
@@ -18,7 +18,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@Ignore
+//@Ignore
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @EnableAutoConfiguration
