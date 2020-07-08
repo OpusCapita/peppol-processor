e217c0856d2e9443bb5450b71ae230a25e06ce12
thebilge <ibrahimbilge7@gmail.com>
2020-07-08 14:41:52 +0200

    adding a local redis server

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 nohup redis-server &
-exec java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -XshowSettings:vm -jar peppol-processor.jar
+java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -XshowSettings:vm -jar peppol-processor.jar
